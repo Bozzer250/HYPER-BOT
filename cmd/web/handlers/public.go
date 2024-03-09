38c94f69fa4870ced7a39def25ae839b2473116d
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2024-03-09 20:14:46 +0200

    added route handlers for public pages

---
+++ cmd/web/handlers/public.go
@@ -0,0 +1,12 @@
+package webHandlers
+
+import (
+	publicPages "hyperbot/cmd/web/pages/public"
+	"hyperbot/utils"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RenderLoginPage(c echo.Context) error {
+	return utils.RenderVIews(c, publicPages.LoginPage())
+}
