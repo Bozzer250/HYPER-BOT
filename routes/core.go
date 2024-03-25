3659e02cd2dc49670859b54214e08111a79a770e
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2024-03-26 00:49:39 +0200

    get the package name from form when creating new account

--- routes/core.go
+++ routes/core.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"fmt"
 	"hyperbot/controllers"
+	"hyperbot/integrations"
 	"hyperbot/utils"
 	publicComponents "hyperbot/web/components/public"
 
@@ -12,9 +13,18 @@ import (
 func PurchaseNewAccount(ctx echo.Context) error {
 	phone := ctx.FormValue("phone_number")
 	referralCode := ctx.FormValue("referral_code")
-	err := controllers.PurchaseNewAccount(phone, referralCode)
+	packageName := ctx.FormValue("package_name")
+	err := controllers.PurchaseNewAccount(phone, referralCode, packageName)
 	if err != nil {
 		return ctx.String(200, fmt.Sprintf("<div>There was an error purchasing the bot.</br>%v</br>Contact support to get help</div>", err))
 	}
 	return utils.RenderVIews(ctx, publicComponents.GoToDashboardButton())
+}
+
+func Test(c echo.Context) error {
+	token, err := integrations.PaypackCashIn(100, "0788711226")
+	if err != nil {
+		return c.String(200, fmt.Sprintf("<div>There was an error cashing in.</br>%v</br>Contact support to get help</div>", err))
+	}
+	return c.String(200, fmt.Sprintf("<div>Token: %v</div>", token))
 }
